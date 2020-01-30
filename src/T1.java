b8bb387b9901955c59dac46ac073dbfa15cfb900
johnhtet <johnhtet@kapps.com.sg>
2020-01-30 17:13:07 +0800

    second commit

--- src/T1.java
+++ src/T1.java
@@ -3,7 +3,7 @@
 public class T1 {
 	public static void main(String[] args) {
 		for(int i=0; i< 10; i++) {
-			System.out.println("Hello World ============>");
+			System.out.println("Hello YGN ============>");
 		}
 	}
 }
